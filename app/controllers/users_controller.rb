06db40b403bec4c57af0f6f12eb56b79d77f24c2
kantapro2020 <kantapro2020@gmail.com>
2020-06-13 18:22:09 +0900

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,16 @@ class UsersController < ApplicationController
 
   def create
     user = User.find_by( name: users_params[:name])
+    if user.update(user_params[:latitude, :longitude])
+      render json: { status: 'SUCCESS', data: User.all }
+    else
+      render json: { status: 'ERROR', data: user.errors }
+    end
+
     unless user
       user = User.new(users_params)
     end
+    
     if user.save
       render json: { status: 'SUCCESS', data: User.all }
     else
@@ -17,9 +24,29 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    
+      
+    end
+  end
+
   private
 
   def users_params
-    params.require(:user).permit(:name, :latitude, :logitude)
+    params.require(:user).permit(:name, :latitude, :longitude)
   end
 end
+
+
+
+
+
+
+
+# def update
+#   if current_user.update(user_params)
+#     redirect_to root_path
+#   else
+#     render :edit
+#   end
+# end
