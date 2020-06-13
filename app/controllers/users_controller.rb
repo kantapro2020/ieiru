6dfd80d904babde6e1687e06712b1c0b1ff96570
kantapro2020 <kantapro2020@gmail.com>
2020-06-13 17:47:30 +0900

    unless_add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(users_params)
+    user = User.find_by( name: users_params[:name])
+    unless user
+      user = User.new(users_params)
+    end
     if user.save
       render json: { status: 'SUCCESS', data: User.all }
     else
       render json: { status: 'ERROR', data: user.errors }
     end
   end
+
+  private
 
   def users_params
     params.require(:user).permit(:name, :latitude, :logitude)
