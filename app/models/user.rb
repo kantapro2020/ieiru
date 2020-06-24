5c1b3b07f15c723226a123b8cb3cb78dcb3c9218
kantapro2020 <kantapro2020@gmail.com>
2020-06-25 01:04:22 +0900

    float

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   before_save :set_is_home
   def ieiru(longitude, latitude)
-    if (longitude - 35.757).abs <= 0.002.abs && (latitude - 139.759).abs <= 0.002
+    if (longitude.to_f - 35.757).abs <= 0.002.abs && (latitude.to_f - 139.759).abs <= 0.002
       return true
     else
       return false
