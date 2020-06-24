90b2d3b4bc305159777d684b5ed7c509a7c2527f
kantapro2020 <kantapro2020@gmail.com>
2020-06-24 22:26:44 +0900

    ieiru_judg

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-  
+  def ieiru(longitude, latitude)
+    if (user.longitude - 35.757).abs <= 0.002.abs && (user.latitude - 139.759).abs <= 0.002
+      return true
+    else
+      return false
+    end
+  end
 end
