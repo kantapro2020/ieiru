6baca7ea5e57ecc880038375a0f970826d2962de
kantapro2020 <kantapro2020@gmail.com>
2020-06-26 01:24:21 +0900

    緯度経度変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   before_save :set_is_home
   def ieiru(longitude, latitude)
-    if (longitude.to_f - 35.757).abs <= 0.002.abs && (latitude.to_f - 139.759).abs <= 0.002
+    if (latitude.to_f - 35.757).abs <= 0.002.abs && (longitude.to_f - 139.759).abs <= 0.002
       return true
     else
       return false
